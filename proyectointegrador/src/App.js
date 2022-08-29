06d0b72bf5ac9f06340d103fad54ea5bb263eb2b
fedefullone <fedefullone@hotmail.com>
2022-08-29 17:12:33 -0300

    inicio componentes header y footer

--- proyectointegrador/src/App.js
+++ proyectointegrador/src/App.js
@@ -1,9 +1,12 @@
 import react from 'react';
-
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 function App() {
   return (
     <div>
       <h1>Hola</h1>
+      <Header/>
+      <Footer/>
     </div>
   );
 }
